88ec0848041ab0bb3412eb10f0b6f39a191b8f32
Sehyeong Kim <kseh92@gmail.com>
2021-02-04 19:13:04 +0900

    전처리 순서 변경

--- example/example.py
+++ example/example.py
@@ -28,10 +28,11 @@ experiment = experiments[0]
 print(experiment)
 print(experiment.get('id'))
 
+# Preprocess before a Run
+client.preprocessor_config_recommend(experiment)
+client.preprocess(experiment)
+
 if experiment.get('status') == 'ready':  # Validation을 여기서 할 건 아닌데, 일단 걸어놓음.
-    # Preprocess before a Run
-    client.preprocessor_config_recommend(experiment)
-    client.preprocess(experiment)
     # Run AutoML
     client.run(experiment)
 else:
