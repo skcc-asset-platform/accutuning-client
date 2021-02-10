797b5ced019e7e51757defdba0e33d148aae0c05
Jeonghun Lee <ahaljh@gmail.com>
2021-02-10 16:23:30 +0900

    Graphql, rest의 클래스 통합, 구조 변경(로그인 관련 내용 이동)

--- accutuning_client/client.py
+++ accutuning_client/client.py
@@ -1,7 +1,6 @@
 from accutuning_client.object import Experiment, Experiments
 import json
-from time import time
-from .util import GraphQL, REST
+from .util import CallApi
 
 
 class Client:
@@ -10,86 +9,18 @@ class Client:
     """
 
     def __init__(self, server_ip, server_port):
-        self._graphql = GraphQL(f'http://{server_ip}:{server_port}/api/graphql')
-        self._rest = REST(f'http://{server_ip}:{server_port}/api')
-
-    def _get_token(self, id, password):
-        """서버에서 토큰을 발급받음"""
-        query = '''
-            mutation tokenAuth($username: String!, $password: String!) {
-                tokenAuth(username: $username, password: $password) {
-                    token
-                }
-            }
-        '''
-        result = self._graphql.execute(query, {'username': id, 'password': password})
-        token = result.get('tokenAuth').get('token')
-        return token
-
-    def _get_token_expire_time(self, token):
-        """발급받은 토큰의 만료시간을 구함"""
-        query = '''
-            mutation verifyToken($token: String!) {
-                verifyToken(token: $token) {
-                    payload
-                }
-            }
-        '''
-        result = self._graphql.execute(query, {'token': token})
-        exp = result.get('verifyToken').get('payload').get('exp')
-        return int(exp)
-
-    def _token_remain_time(self):  # TODO 서버시간, 클라이언트 시간 보정계수 필요?
-        """토큰 만료까지 남은 시간을 초 단위로 리턴함"""
-        return self._token_exp_time - int(time())
-
-    def is_logged_in(self):
-        """로그인 상태인지 구합니다."""
-        return self._token and self._token_remain_time() > 0
-
-    def _refresh_token(self):
-        """토큰을 재발급받음"""
-        query = '''
-            mutation refreshToken($token: String!) {
-                refreshToken(token: $token) {
-                    token
-                    payload
-                }
-            }
-        '''
-        result = self._graphql.execute(query, {'token': self._token})
-        new_token = result.get('refreshToken').get('token')
-        new_exp = int(result.get('refreshToken').get('payload').get('exp'))
-        self._write_token_info(new_token, new_exp)
-
-    def _refresh_token_if_needs(self):
-        """지정 시간 이내로 남을 경우 토큰 재발급 처리"""
-        _TOKEN_REFRESH_MINUTES = 28  # token의 만료시간이 15분 이내로 남으면 리프레시함
-        time_diff = self._token_remain_time()
-        if time_diff < 60 * _TOKEN_REFRESH_MINUTES:
-            print(f'token expire timie이 {_TOKEN_REFRESH_MINUTES}분 이내라 내부적으로 token을 refresh함')  # TODO 메시지는 추후 삭제
-            self._refresh_token()
-
-    def _write_token_info(self, token, token_exp_time):
-        """발급받은 토큰 관련 정보를 기록함"""
-        self._token = token
-        self._token_exp_time = token_exp_time
-        self._rest.add_login_info(token, token_exp_time)
-        self._graphql.add_login_info(token, token_exp_time)
+        self._api = CallApi(server_ip, server_port)
 
     def login(self, id, password):
         """아이디와 비밀번호를 가지고 로그인을 수행함"""
-        token = self._get_token(id, password)
-        token_exp_time = self._get_token_expire_time(token)
-        self._write_token_info(token, token_exp_time)
-        return True
+        return self._api.login(id, password)
 
-    def login_rest(self, id, password):  # TODO graphql 버전 검증 끝나면 삭제예정
-        """아이디와 비밀번호를 가지고 REST 방식으로 로그인을 수행함"""
-        res = self._rest.post('/token-auth/', {'username': id, 'password': password})
-        self._rest.add_login_info(res.get('token'))
-        self._graphql.add_login_info(res.get('token'))
-        return True  # Error 안나고 오면 성공
+    # def login_rest(self, id, password):  # TODO graphql 버전 검증 끝나면 삭제예정
+    #     """아이디와 비밀번호를 가지고 REST 방식으로 로그인을 수행함"""
+    #     res = self._rest.post('/token-auth/', {'username': id, 'password': password})
+    #     self._rest.add_login_info(res.get('token'))
+    #     self._graphql.add_login_info(res.get('token'))
+    #     return True  # Error 안나고 오면 성공
 
     def _server_env(self):
         """서버 환경설정을 가져옵니다."""
@@ -105,7 +36,7 @@ class Client:
                 }
             }
         '''
-        result = self._graphql.execute(query)
+        result = self._api.GRAPHQL(query)
         return result.get('env')
 
     def experiments(self):
@@ -133,11 +64,11 @@ class Client:
                 }
             }
         '''
-        result = self._graphql.execute(query)
+        result = self._api.GRAPHQL(query)
 
-        exps = Experiments(graphql=self._graphql, rest=self._rest)  # TODO 이런 방식으로 셋팅할 수밖에 없을까?
+        exps = Experiments()  # TODO 이런 방식으로 셋팅할 수밖에 없을까? -> list comprehension은 너무 길다.
         for exp_obj in result.get('runtimes'):
-            exp = Experiment(graphql=self._graphql, rest=self._rest, dict_obj=exp_obj)
+            exp = Experiment(api=self._api, dict_obj=exp_obj)
             exps.append(exp)
 
         return exps
@@ -153,23 +84,8 @@ class Client:
         return no_of_container
 
     def create_experiment_from_file(self, filepath):
-        source_str = self._rest.filepost('/sources/workspace_files/', filepath)
+        source_str = self._api.FILEPOST('/sources/workspace_files/', filepath)
         source = json.loads(source_str)
-        result = self._rest.post(f'/sources/{source.get("id")}/experiment/', source)
-        exp = Experiment(graphql=self._graphql, rest=self._rest, dict_obj=result)
+        result = self._api.POST(f'/sources/{source.get("id")}/experiment/', source)
+        exp = Experiment(api=self._api, dict_obj=result)
         return exp
-
-    def sources(self):  # TODO 삭제예정
-        """
-        전체 sources를 가져옴
-        """
-        return self._rest.get('/sources/')
-
-    def create_source_from_file(self, filepath):  # TODO 삭제예정
-        return self._rest.filepost('/sources/workspace_files/', filepath)
-
-    def create_experiment(self, source):  # TODO 삭제예정
-        """
-        입력받은 source를 가지고 실험을 만든다. TODO 이거 Source 객체로 옮길 예정
-        """
-        self._rest.post(f'/sources/{source.get("id")}/experiment/', source)  # TODO 성공/실패 여부 리턴
--- accutuning_client/util.py
+++ accutuning_client/util.py
@@ -1,83 +1,68 @@
 import requests
 import json
+from time import time
 from accutuning_client.exception import HttpStatusError
 
 
-class GraphQL:  # TODO GraphQL과 REST를 통합할 수도 있을까? (겹치는게 좀 있음)
-    """GraphQL 호출을 담당하는 클래스"""
+class CallApi:
+    """GraphQL 및 REST 방식으로 서버를 호출하는 Class"""
 
-    _instance = None
-    _endpoint_url = ''
-    _header = {}
     _token = ''
     _token_exp_time = 0
+    _headers = {}
+    _schema_validation = False  # GraphQL의 Schema Validation으로, 서버에서 보안 때문에 막아놓을 경우 사용이 불가하다.
+    _TOKEN_REFRESH_MINUTES = 28  # token의 만료시간이 15분 이내로 남으면 리프레시함
 
-    def __init__(self, endpoint_url, schema_validation=False):
-        self._endpoint_url = endpoint_url
-        self._schema_validation = schema_validation  # GraphQL의 Schema Validation : 보안때문에 막아놓은 경우 False로 해야함
-        GraphQL._instance = self
+    def __init__(self, server_ip, server_port, graphql_schema_validation=False) -> None:
+        self._graph_endpoint = f'http://{server_ip}:{server_port}/api/graphql'
+        self._rest_api_url = f'http://{server_ip}:{server_port}/api'
+        self._schema_validation = graphql_schema_validation
 
-    def add_login_info(self, token, token_exp_time):  # TODO Graphql과 REST 공통으로 빼자
-        self._token = token
-        self._token_exp_time = token_exp_time
-        self._header['Authorization'] = f'JWT {token}'
+    def login(self, id, password):
+        """입력받은 id, password를 가지고 로그인을 수행합니다."""
+        token = self._get_token(id, password)
+        token_exp_time = self._get_token_expire_time(token)
+        self._write_token_info(token, token_exp_time)
+        return True  # 에러가 없으면 성공
 
-    async def execute_async(self, query, param={}):
-        from gql import Client, gql
-        from gql.transport.aiohttp import AIOHTTPTransport
+    def is_logged_in(self):
+        """로그인 상태인지 구합니다."""
+        return self._token and self._token_remain_time() > 0
 
-        transport = AIOHTTPTransport(url=self._endpoint_url, headers=self._header)
+    def GRAPHQL(self, query, params={}):  # GET Naming에 맞춰 대문자로 생성
+        """GraphQL 방식으로 호출합니다."""
+        # Token refresh Logic
+        if self.is_logged_in() and self._token_remain_time() < 60 * CallApi._TOKEN_REFRESH_MINUTES:
+            print(f'token expire timie이 {CallApi._TOKEN_REFRESH_MINUTES}분 이내라 내부적으로 token을 refresh함')  # TODO 메시지는 추후 삭제
+            self._refresh_token()
 
-        # Using `async with` on the client will start a connection on the transport
-        # and provide a `session` variable to execute queries on this connection
-        async with Client(
-            transport=transport, fetch_schema_from_transport=self._schema_validation,
-        ) as session:
-            result = await session.execute(gql(query), variable_values=param)
-            return result
+        return self._GRAPHQL(query, params)
 
-    def execute(self, query, param={}):
-        # return asyncio.run(self.execute_async(query, param))
+    def _GRAPHQL(self, query, params={}):
+        """GraphQL 방식으로 호출합니다."""
         from gql import Client, gql
         from gql.transport.requests import RequestsHTTPTransport
 
         transport = RequestsHTTPTransport(
-            url=self._endpoint_url, verify=True, retries=3, headers=self._header
+            url=self._graph_endpoint, verify=True, retries=3, headers=self._headers
         )
 
         client = Client(transport=transport, fetch_schema_from_transport=self._schema_validation)
-        result = client.execute(gql(query), variable_values=param)
+        result = client.execute(gql(query), variable_values=params)
         return result
 
-
-# REST는 이렇게 씌울 필요가 있을까, 그냥 requests에서 바로 가져다 쓰는 것은...
-class REST:
-    """REST API를 호출하는 역할을 진행하는 클래스"""
-
-    _instance = None
-    _header = {}
-    _token = ''
-    _token_exp_time = 0
-
-    def __init__(self, api_url):
-        self._api_url = api_url
-        REST._instance = self
-
-    def add_login_info(self, token, token_exp_time):
-        self._token = token
-        self._token_exp_time = token_exp_time
-        self._header['Authorization'] = f'JWT {token}'
-
-    def get(self, url):
-        res = requests.get(self._api_url + url, headers=self._header)
+    def GET(self, url):  # dictionary에서 쓰는 get과 헷갈릴 수 있어 대문자로 생성함
+        """REST API의 GET 방식으로 서버를 호출합니다."""
+        res = requests.get(self._rest_api_url + url, headers=self._headers)
         if res.ok:
             obj = json.loads(res.text)
             return obj
         else:
             raise HttpStatusError(res.status_code, res.text)
 
-    def post(self, url, param):
-        res = requests.post(self._api_url + url, data=param, headers=self._header)
+    def POST(self, url, params):  # GET Naming에 맞춰 대문자로 생성
+        """REST API의 POST 방식으로 서버를 호출합니다."""
+        res = requests.post(self._rest_api_url + url, data=params, headers=self._headers)
         result = ''
         if res.ok:
             result = json.loads(res.text)  # TODO 여기 관련 에러처리
@@ -86,16 +71,166 @@ class REST:
 
         return result
 
-    def filepost(self, url, filepath, param={}):
+    def FILEPOST(self, url, filepath, params={}):  # GET Naming에 맞춰 대문자로 생성
+        """REST API의 POST 방식으로 서버에 파일을 업로드합니다."""
         from pathlib import Path
         filename = Path(filepath).name
 
         result = ''
         with open(filepath, 'rb') as f:
-            res = requests.post(self._api_url + url, files={'fileData': (filename, f, 'text/csv')}, data=param, headers=self._header)
+            res = requests.post(self._rest_api_url + url, files={'fileData': (filename, f, 'text/csv')}, data=params, headers=self._headers)
             if res.ok:
                 result = res.text
             else:
                 raise HttpStatusError(res.status_code, res.text)
 
         return result
+
+    def _get_token(self, id, password):
+        """id와 password를 가지고 서버에서 토큰을 발급받습니다."""
+        query = '''
+            mutation tokenAuth($username: String!, $password: String!) {
+                tokenAuth(username: $username, password: $password) {
+                    token
+                }
+            }
+        '''
+        result = self._GRAPHQL(query, {'username': id, 'password': password})
+        token = result.get('tokenAuth').get('token')
+        return token
+
+    def _get_token_expire_time(self, token):
+        """입력받은 토큰의 만료시간을 구합니다."""
+        query = '''
+            mutation verifyToken($token: String!) {
+                verifyToken(token: $token) {
+                    payload
+                }
+            }
+        '''
+        result = self._GRAPHQL(query, {'token': token})
+        exp = result.get('verifyToken').get('payload').get('exp')
+        return int(exp)
+
+    def _refresh_token(self):
+        """서버에서 토큰을 재발급받습니다."""
+        query = '''
+            mutation refreshToken($token: String!) {
+                refreshToken(token: $token) {
+                    token
+                    payload
+                }
+            }
+        '''
+        result = self._GRAPHQL(query, {'token': self._token})
+        new_token = result.get('refreshToken').get('token')
+        new_exp = int(result.get('refreshToken').get('payload').get('exp'))
+        self._write_token_info(new_token, new_exp)
+
+    def _token_remain_time(self):  # TODO 서버시간, 클라이언트 시간 보정계수 필요?
+        """토큰 만료까지 남은 시간을 초 단위로 리턴합니다."""
+        return self._token_exp_time - int(time())
+
+    def _write_token_info(self, token, token_exp_time):
+        """발급받은 토큰 관련 정보를 기록합니다."""
+        self._token = token
+        self._token_exp_time = token_exp_time
+        self._headers = {'Authorization': f'JWT {token}'}
+
+
+# class GraphQL:  # TODO GraphQL과 REST를 통합할 수도 있을까? (겹치는게 좀 있음)
+#     """GraphQL 호출을 담당하는 클래스"""
+
+#     _instance = None
+#     _endpoint_url = ''
+#     _header = {}
+#     _token = ''
+#     _token_exp_time = 0
+
+#     def __init__(self, endpoint_url, schema_validation=False):
+#         self._endpoint_url = endpoint_url
+#         self._schema_validation = schema_validation  # GraphQL의 Schema Validation : 보안때문에 막아놓은 경우 False로 해야함
+#         GraphQL._instance = self
+
+#     def add_login_info(self, token, token_exp_time):  # TODO Graphql과 REST 공통으로 빼자
+#         self._token = token
+#         self._token_exp_time = token_exp_time
+#         self._header['Authorization'] = f'JWT {token}'
+
+#     async def execute_async(self, query, param={}):
+#         from gql import Client, gql
+#         from gql.transport.aiohttp import AIOHTTPTransport
+
+#         transport = AIOHTTPTransport(url=self._endpoint_url, headers=self._header)
+
+#         # Using `async with` on the client will start a connection on the transport
+#         # and provide a `session` variable to execute queries on this connection
+#         async with Client(
+#             transport=transport, fetch_schema_from_transport=self._schema_validation,
+#         ) as session:
+#             result = await session.execute(gql(query), variable_values=param)
+#             return result
+
+#     def execute(self, query, param={}):
+#         # return asyncio.run(self.execute_async(query, param))
+#         from gql import Client, gql
+#         from gql.transport.requests import RequestsHTTPTransport
+
+#         transport = RequestsHTTPTransport(
+#             url=self._endpoint_url, verify=True, retries=3, headers=self._header
+#         )
+
+#         client = Client(transport=transport, fetch_schema_from_transport=self._schema_validation)
+#         result = client.execute(gql(query), variable_values=param)
+#         return result
+
+
+# # REST는 이렇게 씌울 필요가 있을까, 그냥 requests에서 바로 가져다 쓰는 것은...
+# class REST:
+#     """REST API를 호출하는 역할을 진행하는 클래스"""
+
+#     _instance = None
+#     _header = {}
+#     _token = ''
+#     _token_exp_time = 0
+
+#     def __init__(self, api_url):
+#         self._api_url = api_url
+#         REST._instance = self
+
+#     def add_login_info(self, token, token_exp_time):
+#         self._token = token
+#         self._token_exp_time = token_exp_time
+#         self._header['Authorization'] = f'JWT {token}'
+
+#     def get(self, url):
+#         res = requests.get(self._api_url + url, headers=self._header)
+#         if res.ok:
+#             obj = json.loads(res.text)
+#             return obj
+#         else:
+#             raise HttpStatusError(res.status_code, res.text)
+
+#     def post(self, url, param):
+#         res = requests.post(self._api_url + url, data=param, headers=self._header)
+#         result = ''
+#         if res.ok:
+#             result = json.loads(res.text)  # TODO 여기 관련 에러처리
+#         else:
+#             raise HttpStatusError(res.status_code, res.text)
+
+#         return result
+
+#     def filepost(self, url, filepath, param={}):
+#         from pathlib import Path
+#         filename = Path(filepath).name
+
+#         result = ''
+#         with open(filepath, 'rb') as f:
+#             res = requests.post(self._api_url + url, files={'fileData': (filename, f, 'text/csv')}, data=param, headers=self._header)
+#             if res.ok:
+#                 result = res.text
+#             else:
+#                 raise HttpStatusError(res.status_code, res.text)
+
+#         return result
